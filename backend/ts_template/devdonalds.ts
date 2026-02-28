14a0d6e93fd68cbed626aab43c956ceb006e8015
Briyan <z5463890@ad.unsw.edu.au>
2026-02-28 15:51:01 +1100

    add part 2 of backend of Typescript

--- backend/ts_template/devdonalds.ts
+++ backend/ts_template/devdonalds.ts
@@ -26,20 +26,20 @@ const app = express();
 app.use(express.json());
 
 // Store your recipes here!
-const cookbook: any = null;
+const cookbook: Map<string, cookbookEntry> = new Map(); // cookbookEntry = null;
 
 // Task 1 helper (don't touch)
-app.post("/parse", (req:Request, res:Response) => {
+app.post("/parse", (req: Request, res: Response) => {
   const { input } = req.body;
 
   const parsed_string = parse_handwriting(input)
   if (parsed_string == null) {
     res.status(400).send("this string is cooked");
     return;
-  } 
+  }
   res.json({ msg: parsed_string });
   return;
-  
+
 });
 
 // [TASK 1] ====================================================================
@@ -63,21 +63,87 @@ const parse_handwriting = (recipeName: string): string | null => {
   if (recipeName.length == 0) {
     return null;
   }
-  
+
+  const mapAsObject = Object.fromEntries(cookbook);
+  console.log(JSON.stringify(mapAsObject, null, 2));
+
   return recipeName
 }
 
 // [TASK 2] ====================================================================
 // Endpoint that adds a CookbookEntry to your magical cookbook
-app.post("/entry", (req:Request, res:Response) => {
-  // TODO: implement me
-  res.status(500).send("not yet implemented!")
+app.post("/entry", (req: Request, res: Response) => {
 
+  const data = req.body;
+  console.log(data);
+  const food_name: string = data.name;
+  const food_type: string = data.type;
+
+  if (food_type != "ingredient" && food_type != "recipe") {
+    res.status(400).send("invalid type");
+    return;
+  }
+
+  let cook_time = -1;
+  if (food_type == "ingredient") {
+    cook_time = data.cookTime;
+    if (cook_time < 0) {
+      res.status(400).send("invalid cook time");
+      return;
+    }
+  }
+
+  if (cookbook.has(food_name)) {
+    res.status(400).send("entry already exists");
+    return;
+  }
+
+  let list_items: requiredItem[] = [];
+  let seen_array: string[] = [];
+  if (food_type == "recipe") {
+    for (let item of data.requiredItems) {
+
+      if (item.name in seen_array) {
+        res.status(400).send("duplicate item");
+        return;
+      }
+
+      if (item.quantity < 0) {
+        res.status(400).send("invalid quantity");
+        return;
+      }
+
+      let required_item: requiredItem = {
+        name: item.name,
+        quantity: item.quantity
+      }
+
+      seen_array.push(item.name);
+      list_items.push(required_item);
+    }
+  }
+
+  if (food_type == "ingredient") {
+    const new_ingredient: ingredient = {
+      name: food_name,
+      type: food_type,
+      cookTime: cook_time
+    }
+    cookbook.set(food_name, new_ingredient);
+  } else {
+    const new_recipe: recipe = {
+      name: food_name,
+      type: food_type,
+      requiredItems: list_items
+    }
+    cookbook.set(food_name, new_recipe);
+  }
+  res.status(200).send({});
 });
 
 // [TASK 3] ====================================================================
 // Endpoint that returns a summary of a recipe that corresponds to a query name
-app.get("/summary", (req:Request, res:Request) => {
+app.get("/summary", (req: Request, res: Request) => {
   // TODO: implement me
   res.status(500).send("not yet implemented!")
 
